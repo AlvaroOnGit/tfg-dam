e1ef7d3273f06860428eced6e4f251de9092c668
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-02-17 19:25:54 +0100

    Added the view controllers aswell as an initial endpoint to render the index page
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- routes/web/view.routes.js
+++ routes/web/view.routes.js
@@ -1,1 +1,15 @@
-// Handles routing for rendering views
+/**
+ * Handles routing for rendering views
+ */
+import { Router } from 'express';
+import { ViewController } from '../../controllers/web/view.controller.js';
+
+export const createViewRouter = () => {
+
+    const viewRouter = Router();
+    const viewController = new ViewController();
+
+    viewRouter.get('/', viewController.index);
+
+    return viewRouter;
+}
