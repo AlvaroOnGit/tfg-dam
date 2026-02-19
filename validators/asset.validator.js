d4340c7c06eaa465b1f1212fd906c3638163574c
ionut.barany <ionut7ag@gmail.com>
2026-02-19 18:04:40 +0100

    Implementacion de la validacion

--- validators/asset.validator.js
+++ validators/asset.validator.js
@@ -1,3 +1,35 @@
 /**
  * Contains logic to validate assets for different games
  */
+import {
+    WeaponSchema,
+    ArmorSchema,
+    TalismanSchema,
+    AshOfWarSchema,
+    SpiritAshSchema,
+} from '../schemas/games/elden-ring.schemas.js';
+
+// Mapa de esquemas según el tipo de asset
+const assetSchemas = {
+    weapon: WeaponSchema,
+    armor: ArmorSchema,
+    talisman: TalismanSchema,
+    ashOfWar: AshOfWarSchema,
+    spiritAsh: SpiritAshSchema,
+};
+
+// Función para validar el asset
+const validateAsset = (gameId, assetType, data) => {
+    if (gameId !== "elden_ring") {
+        throw new Error("Invalid game ID");
+    }
+
+    const schema = assetSchemas[assetType];
+    if (!schema) {
+        throw new Error("Invalid asset type");
+    }
+
+    return schema.parse(data);
+};
+
+export default validateAsset;
