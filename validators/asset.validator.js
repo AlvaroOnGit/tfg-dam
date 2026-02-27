8ff66e9e74400a4114bd867e9517b11ecf3094b8
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-02-27 19:33:25 +0100

    First pass of asset validator
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- validators/asset.validator.js
+++ validators/asset.validator.js
@@ -1,3 +1,27 @@
 /**
  * Contains logic to validate assets for different games
  */
+
+import EldenRingSchema from '../schemas/games/elden_ring/asset.schemas.js'
+import TaintedGrailSchema from '../schemas/games/tainted_grail/asset.schemas.js';
+
+const gameSchemas = {
+    "elden-ring": EldenRingSchema,
+    "tainted-grail": TaintedGrailSchema
+}
+
+export async function validateAsset(data) {
+
+    const { gameSlug } = data
+
+    const gameSchema = gameSchemas[gameSlug];
+
+    if (!gameSchema) {
+        return {
+            success: false,
+            error: `Unsupported game: ${gameSlug}`
+        };
+    }
+
+    return await gameSchema.safeParse(data);
+}
