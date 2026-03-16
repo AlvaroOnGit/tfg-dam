145e79441c4306d28f16c8071e9a0efa4827d8f1
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-03-16 16:36:29 +0100

    Implemented barrel file to condense all validators in a single import.
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

---
+++ validators/index.js
@@ -0,0 +1,15 @@
+/**
+ * @module validators
+ * Barrel file that re-exports all validators.
+ *
+ * @example
+ * // Import all
+ * import * as validators from './validators/index.js';
+ *
+ * // Import individually
+ * import { validateAsset, validateBuild, ... } from './validators/index.js';
+ */
+export { validateAsset } from './asset.validator.js';
+export { validateBuild } from './build.validator.js';
+export { validateGame } from './game.validator.js';
+export { validateUser } from './user.validator.js';
