c545994bbf1b775fc7cfb46c596182d9de001e78
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-03-16 16:32:26 +0100

    Simplified schema and corrected typos.
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- schemas/games/elden_ring/build.schemas.js
+++ schemas/games/elden_ring/build.schemas.js
@@ -1,10 +1,13 @@
+/**
+ * Contains logic to validate builds for Elden Ring
+ */
+
 import { z } from 'zod';
 import createBaseBuildSchema from "../../builds.schemas.js";
 
 const BaseSchema = createBaseBuildSchema('elden-ring');
 
 //Enums
-
 const buildTags = z.enum([
     "beginner-friendly",
     "endgame",
@@ -32,38 +35,29 @@ const buildTags = z.enum([
     "no-hit",
 ]);
 
-//Temple data schema
+// ─── Template Data Schema ───────────────────────────────────────────────────────────
 
-const templateDataSchema = z.object({
-    ashOfWar1: z
-        .string("ashOfWar1 must be a string")
-        .trim()
-        .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "slug must be lowercase letters, numbers and single dashes only")
-        .min(1, "ashOfWar1 must be at least 1 character long")
-        .max(100, "ashOfWar1 cannot exceed 100 characters")
-        .nullable(),
-    ashOfWar2: z
-        .string("ashOfWar2 must be a string")
-        .trim()
-        .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "slug must be lowercase letters, numbers and single dashes only")
-        .min(1, "ashOfWar2 must be at least 1 character long")
-        .max(100, "ashOfWar2 cannot exceed 100 characters")
-        .nullable(),
-    ashOfWar3: z
-        .string("ashOfWar3 must be a string")
-        .trim()
-        .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "slug must be lowercase letters, numbers and single dashes only")
-        .min(1, "ashOfWar3 must be at least 1 character long")
-        .max(100, "ashOfWar3 cannot exceed 100 characters")
-        .nullable(),
-})
+const ashOfWarSlots = ["ashOfWar1", "ashOfWar2", "ashOfWar3"];
+
+const templateDataSchema = z.object(
+    Object.fromEntries(
+        ashOfWarSlots.map(type => [type, z
+            .string(`${type} must be a slug`)
+            .trim()
+            .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "slug must be lowercase letters, numbers and single dashes only")
+            .min(1, `${type} must be at least 1 character long`)
+            .max(100, `${type} cannot exceed 100 characters`)
+            .nullable(),
+        ])
+    )
+);
 
 // ─── Main Schema ────────────────────────────────────────────────────────────────
 
 const EldenRingBuildSchema =
     BaseSchema.extend({
             tags: z.array(buildTags),
-            template_data: templateDataSchema
+            templateData: templateDataSchema
         }
     );
 
