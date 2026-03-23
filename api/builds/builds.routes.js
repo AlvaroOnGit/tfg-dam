c25894d6c985f1eb56e63ab19e1d1b1943ee4cc5
Luca Morlando <lucam4523@gmail.com>
2026-03-23 20:33:20 +0100

    Rebased dev changes into fork

--- api/builds/builds.routes.js
+++ api/builds/builds.routes.js
@@ -1,16 +1,47 @@
 /**
  * Routing related to the build API
- *
  */
-import { Router } from "express";
+import { Router } from 'express';
 import { BuildController } from './builds.controller.js';
 import { BuildService } from './builds.service.js';
+import { validationHandler } from '../../shared/middlewares/validation.middleware.js';
+import { authHandler } from '../../shared/middlewares/auth.middleware.js';
+import { validateBuild, validateBuildQuery, validateBuildId } from '../../shared/validators/index.js';
 
-export const createBuildRouter = ({}) => {
+export const createBuildRouter = ({ BuildModel }) => {
 
     const buildRouter = Router();
-    const buildService = new BuildService({});
+    const buildService = new BuildService({ BuildModel });
     const buildController = new BuildController({ buildService });
+
+    buildRouter.get('/',
+        validationHandler(validateBuildQuery, 'query'),
+        buildController.getAll
+    );
+
+    buildRouter.get('/:id',
+        validationHandler(validateBuildId, 'params'),
+        buildController.getById
+    );
+
+    buildRouter.post('/',
+        authHandler,
+        validationHandler(validateBuild),
+        buildController.create
+    );
+
+    buildRouter.patch('/:id',
+        authHandler,
+        validationHandler(validateBuildId, 'params'),
+        validationHandler(validateBuild),
+        buildController.update
+    );
+
+    buildRouter.delete('/:id',
+        authHandler,
+        validationHandler(validateBuildId, 'params'),
+        buildController.remove
+    );
 
     return buildRouter;
 }
--- shared/validators/index.js
+++ shared/validators/index.js
@@ -10,7 +10,7 @@
  * import { validateAsset, validateBuild, ... } from './validators/index.js';
  */
 export { validateAsset, validateAssetQuery, validateAssetId } from './asset.validator.js';
-export { validateBuild } from './build.validator.js';
+export { validateBuild, validateBuildQuery, validateBuildId } from './build.validator.js';
 export { validateGame } from './game.validator.js';
 export { validateUser } from './user.validator.js';
 export { validateLogin, validateRegister, validatePartialAuth } from './auth.validator.js';
