45105e10d7d900182481d3982443bc935d428a20
Luca Morlando <lucam4523@gmail.com>
2026-03-23 20:33:20 +0100

    wip

--- api/builds/builds.controller.js
+++ api/builds/builds.controller.js
@@ -1,7 +1,54 @@
 export class BuildController {
-    constructor({ buildService } = {}) {
-        this.buildService = buildService;
+    constructor({buildService} = {}) {
+        this.buildsService = buildService;
     }
 
+    getAll = async (req, res, next) => {
+        try {
+            const builds = await this.buildsService.getAll();
+            res.status(200).json(builds);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    getById = async (req, res, next) => {
+        try {
+            const {id} = req.params;
+            const build = await this.buildsService.getById(id);
+            res.status(200).json(build);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    create = async (req, res, next) => {
+        try {
+            const build = await this.buildsService.createBuild(req.body);
+            res.status(201).json(build);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    update = async (req, res, next) => {
+        try {
+            const {id} = req.params;
+            const build = await this.buildsService.updateBuild(id, req.body);
+            res.status(200).json(build);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    remove = async (req, res, next) => {
+        try {
+            const {id} = req.params;
+            await this.buildsService.deleteBuild(id);
+            res.status(204).send();
+        } catch (e) {
+            next(e);
+        }
+    }
 
 }
