812beaad2614efdb9e1b7331fa58fe63adbe4b39
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-04-13 21:41:59 +0200

    Added permission related functions
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- api/builds/builds.service.js
+++ api/builds/builds.service.js
@@ -1,25 +1,33 @@
-import {ForbiddenError, InternalError, NotFoundError} from "../../shared/middlewares/error.middleware.js";
+import {
+    ConflictError,
+    InternalError,
+    NotFoundError,
+
+} from "../../shared/middlewares/error.middleware.js";
 
 export class BuildService {
 
-    constructor({ BuildModel } = {}) {
+    constructor({ BuildModel, UserModel } = {}) {
         this.buildModel = BuildModel;
+        this.userModel = UserModel;
     }
 
-    getAll = async (filters) => {
+    getAllBuilds = async (filters) => {
         try {
-            return await this.buildModel.findAll(filters);
+            return await this.buildModel.findAllBuilds(filters);
         } catch (e) {
             throw new InternalError('Failed to fetch builds', e);
         }
     }
 
-    getById = async (id) => {
+    //TODO: Parse template_data according to each game?
+    // Do independent api calls to parse the remaining assets on frontend
+    getBuildById = async (id) => {
         let build;
         try {
-            build = await this.buildModel.findById(id);
+            build = await this.buildModel.findBuildById(id);
         } catch (e) {
-            throw new InternalError('Failed to fetch build', e);
+            throw new InternalError('Failed to fetch build data', e);
         }
         if (!build) {
             throw new NotFoundError('Build not found');
@@ -27,69 +35,78 @@ export class BuildService {
         return build;
     }
 
-    createBuild = async (buildData, userId) => {
+    createBuild = async (data, userId) => {
+        const { assets, ...buildData } = data;
         const build = { ...buildData, creatorId: userId };
 
         try {
-            return await this.buildModel.create(build);
+            return await this.buildModel.createBuild(build, assets);
         } catch (e) {
             throw new InternalError('Failed to create build', e);
         }
     }
 
-    updateBuild = async (id, buildData, userId) => {
-        let existing;
-        try {
-            existing = await this.buildModel.findById(id);
-        } catch (e) {
-            throw new InternalError('Failed to fetch build', e);
-        }
-        if (!existing) {
-            throw new NotFoundError('Build not found');
-        }
-
-        let hasPermission;
-        try {
-            hasPermission = await this.buildModel.hasEditPermission(id, userId);
-        } catch (e) {
-            throw new InternalError('Failed to check permissions', e);
-        }
-        if (!hasPermission) {
-            throw new ForbiddenError('You do not have permission to edit this build');
-        }
+    updateBuild = async (id, data) => {
+        const { assets, ...buildData } = data;
 
         try {
-            return await this.buildModel.update(id, buildData);
+            return await this.buildModel.updateBuild(id, buildData, assets);
         } catch (e) {
             throw new InternalError('Failed to update build', e);
         }
     }
 
-    deleteBuild = async (id, userId) => {
-        let existing;
+    deleteBuild = async (id) => {
         try {
-            existing = await this.buildModel.findById(id);
-        } catch (e) {
-            throw new InternalError('Failed to fetch build', e);
-        }
-        if (!existing) {
-            throw new NotFoundError('Build not found');
-        }
-
-        let hasPermission;
-        try {
-            hasPermission = await this.buildModel.hasEditPermission(id, userId);
-        } catch (e) {
-            throw new InternalError('Failed to check permissions', e);
-        }
-        if (!hasPermission) {
-            throw new ForbiddenError('You do not have permission to delete this build');
-        }
-
-        try {
-            await this.buildModel.delete(id);
+            await this.buildModel.deleteBuild(id);
         } catch (e) {
             throw new InternalError('Failed to delete build', e);
         }
+    }
+
+    getEditPermission = async (id) => {
+        let permissions;
+        try {
+            permissions = await this.buildModel.getBuildPermissions(id);
+        } catch (e) {
+            throw new InternalError('Failed to check permissions', e);
+        }
+        if (permissions.length === 0) {
+            throw new NotFoundError('Build not found');
+        }
+        return permissions;
+    }
+
+    grantEditPermission = async (id, userId, role) => {
+        let user;
+        try {
+            user = await this.userModel.findById(userId);
+        } catch (e) {
+            throw new InternalError('Could not fetch user');
+        }
+        if (!user) {
+            throw new NotFoundError('User not found');
+        }
+
+        try {
+            return await this.buildModel.grantEditPermission(id, userId, role);
+        } catch (e) {
+            if (e.code === 'P0001') throw new ConflictError(e.message);
+            if (e.code === '23505') throw new ConflictError('User already has permissions for this build');
+            throw new InternalError('Failed to add build permissions', e);
+        }
+    }
+
+    revokeEditPermission = async (id, userId) => {
+        let deleted;
+        try {
+            deleted = await this.buildModel.revokeEditPermission(id, userId);
+        } catch (e) {
+            throw new InternalError('Failed to revoke build permissions', e);
+        }
+        if (!deleted) {
+            throw new NotFoundError('Editor not found');
+        }
+        return deleted;
     }
 }
