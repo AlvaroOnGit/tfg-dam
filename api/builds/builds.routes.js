9ca4d50e342606e617477a350b78444ca9db3081
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-04-13 21:41:05 +0200

    Refactored imports. Endpoints now use the new middlewares.
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- api/builds/builds.routes.js
+++ api/builds/builds.routes.js
@@ -5,44 +5,62 @@ import { Router } from 'express';
 import { BuildController } from './builds.controller.js';
 import { BuildService } from './builds.service.js';
 import { validationHandler } from '../../shared/middlewares/validation.middleware.js';
+import { permissionHandler } from "../../shared/middlewares/permission.middleware.js";
 import { authHandler } from '../../shared/middlewares/auth.middleware.js';
-import { validateBuild, validateBuildQuery, validateBuildId } from '../../shared/validators/index.js';
-import {validateBuildUpdate} from "../../shared/validators/build.validator.js";
+import { buildParser } from "../../shared/middlewares/parser.middleware.js";
+import {
+    validateBuildParams,
+    validateBuildQuery,
+    validateBuild,
+    validateBuildPartial
+} from '../../shared/validators/index.js';
 
-export const createBuildRouter = ({ BuildModel }) => {
+export const createBuildRouter = ({ BuildModel, UserModel, AssetModel }) => {
 
     const buildRouter = Router();
-    const buildService = new BuildService({ BuildModel });
+    const buildService = new BuildService({ BuildModel, UserModel });
     const buildController = new BuildController({ buildService });
 
     buildRouter.get('/',
         validationHandler(validateBuildQuery, 'query'),
-        buildController.getAll
-    );
-
+        buildController.getAllBuilds);
     buildRouter.get('/:id',
-        validationHandler(validateBuildId, 'params'),
-        buildController.getById
-    );
-
+        validationHandler(validateBuildParams, 'params'),
+        buildController.getBuildById,
+        buildParser(AssetModel)
+        );
     buildRouter.post('/',
         authHandler,
         validationHandler(validateBuild),
-        buildController.create
+        buildController.createBuild,
+        buildParser(AssetModel)
     );
-
     buildRouter.patch('/:id',
         authHandler,
-        validationHandler(validateBuildId, 'params'),
-        validationHandler(validateBuildUpdate),
-        buildController.update
-    );
-
+        permissionHandler(BuildModel),
+        validationHandler(validateBuildParams, 'params'),
+        validationHandler(validateBuildPartial),
+        buildController.updateBuild,
+        buildParser(AssetModel)
+        );
     buildRouter.delete('/:id',
         authHandler,
-        validationHandler(validateBuildId, 'params'),
-        buildController.remove
-    );
+        permissionHandler(BuildModel),
+        validationHandler(validateBuildParams, 'params'),
+        buildController.deleteBuild);
+    buildRouter.get('/:id/editors',
+        validationHandler(validateBuildParams, 'params'),
+        buildController.getEditPermission);
+    buildRouter.post('/:id/editors',
+        authHandler,
+        permissionHandler(BuildModel),
+        validationHandler(validateBuildParams, 'params'),
+        buildController.grantEditPermission);
+    buildRouter.delete('/:id/editors/:userId',
+        authHandler,
+        permissionHandler(BuildModel),
+        validationHandler(validateBuildParams, 'params'),
+        buildController.revokeEditPermission);
 
     return buildRouter;
 }
