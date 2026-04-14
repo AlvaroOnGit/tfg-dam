3ad43a0c87586fb1aca5e637267fd87414295cc5
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-04-14 17:08:32 +0200

    Implemented leftover functions to update the users profile
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -1,24 +1,64 @@
 export class UserController {
+
     constructor({ userService } = {}) {
         this.userService = userService;
     }
 
+    #clearAuth = (res, message) => {
+        res
+            .status(200)
+            .clearCookie('access_token')
+            .clearCookie('refresh_token')
+            .json({ message });
+    }
+
     getMyProfile = async (req, res, next) => {
         try {
-            const requestUser = req.user ?? req.session?.user ?? { id: req.session?.userId };
-            console.log('Request user:', requestUser);
-            const profile = await this.userService.getMyProfile(requestUser);
-            res.status(200).json(profile);
+            const user = await this.userService.getMyProfile(req.user);
+            res.status(200).json(user);
         } catch (e) {
             next(e);
         }
     }
 
     getPublicProfile = async (req, res, next) => {
+        const { id } = req.params;
         try {
-            const { id } = req.params;
-            const profile = await this.userService.getPublicProfileById(id);
-            res.status(200).json(profile);
+            const user = await this.userService.getProfileById(id);
+            res.status(200).json(user);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    updateEmail = async (req, res, next) => {
+        const { email, password } = req.body;
+        const { id } = req.user;
+        try {
+            await this.userService.updateEmail(id, password, email);
+            this.#clearAuth(res, 'Email updated successfully.');
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    updatePassword = async (req, res, next) => {
+        const { newPassword, password } = req.body;
+        const { id } = req.user;
+        try {
+            await this.userService.updatePassword(id, password, newPassword);
+            this.#clearAuth(res, 'Password updated successfully.');
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    updateUsername = async (req, res, next) => {
+        const { username, password } = req.body;
+        const { id } = req.user;
+        try {
+            await this.userService.updateUsername(id, password, username);
+            this.#clearAuth(res, 'Username updated successfully.');
         } catch (e) {
             next(e);
         }
