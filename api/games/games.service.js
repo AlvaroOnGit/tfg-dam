665e8f27ae0bbad13350fecb5059a7020de9c4e8
Kiko Lista <kikolista7@gmail.com>
2026-04-14 11:21:40 +0200

    Merge pull request #47
    * Actualización
    
    * Merge branch 'dev' into 25-crear-endpoints-de-games

--- api/games/games.service.js
+++ api/games/games.service.js
@@ -1,5 +1,15 @@
+import { GameModel } from '../../shared/models/game.model.js';
+
 export class GameService {
     constructor({} = {}) {
 
+    }
+
+    async getGames(params) {
+        return await GameModel.getGames(params);
+    }
+
+    async getGameBySlug(slug) {
+        return await GameModel.getGameBySlug(slug);
     }
 }
