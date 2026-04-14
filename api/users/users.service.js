6ca256f275d4bb19ca2ca3915925fbd86beefb09
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-04-14 17:09:25 +0200

    Refactored to accommodate user profile updates
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- api/users/users.service.js
+++ api/users/users.service.js
@@ -1,63 +1,107 @@
-import { AuthenticationError, InternalError, NotFoundError } from '../../shared/middlewares/error.middleware.js';
-
-/*export class UserService {
-    constructor({ UserModel } = {}) {
-        this.userModel = UserModel;
-    }
-
-    getMyProfile = async (requestUser) => {
-        const userId = requestUser?.id;
-        if (!userId) {
-            throw new AuthenticationError('User not authenticated');
-        }
-
-        try {
-            const profile = await this.userModel.findPrivateProfileById(userId);
-            if (!profile) {
-                throw new NotFoundError('User not found');
-            }
-            return profile;
-        } catch (e) {
-            if (e instanceof AuthenticationError) {
-                throw e;
-            }
-            throw new InternalError('Failed to fetch user profile', e);
-        }
-    }
-
-    getPublicProfileById = async (id) => {
-        try {
-            const profile = await this.userModel.findPublicProfileById(id);
-            if (!profile) {
-                throw new NotFoundError('User not found');
-            }
-            return profile;
-        } catch (e) {
-            if (e instanceof NotFoundError) {
-                throw e;
-            }
-            throw new InternalError('Failed to fetch user profile', e);
-        }
-    }
-}*/
+import {
+    AuthenticationError,
+    ConflictError,
+    InternalError,
+    NotFoundError
+} from '../../shared/middlewares/error.middleware.js';
+import argon2 from 'argon2';
 
 export class UserService {
     constructor({ UserModel } = {}) {
         this.userModel = UserModel;
     }
 
-    getMyProfile = async (requestUser) => {
-        const userId = requestUser?.id;
-        if (!userId) throw new AuthenticationError('User not authenticated');
+    #fetchAndVerifyUser = async (id, password) => {
+        let userCredentials;
+        try {
+            userCredentials = await this.userModel.findById(id);
+        } catch (e) {
+            throw new InternalError('Could not fetch user', e);
+        }
+        if (!userCredentials) {
+            throw new NotFoundError('User not found');
+        }
 
-        const profile = await this.userModel.findPrivateProfileById(userId);
-        if (!profile) throw new NotFoundError('User not found');
-        return profile;
+        const isCurrentPasswordValid = await argon2.verify(userCredentials.password, password);
+        if (!isCurrentPasswordValid) {
+            throw new AuthenticationError('Invalid password');
+        }
+
+        return userCredentials;
     }
 
-    getPublicProfileById = async (id) => {
-        const profile = await this.userModel.findPublicProfileById(id);
-        if (!profile) throw new NotFoundError('User not found');
-        return profile;
+    getMyProfile = async (user) => {
+        const { id } = user;
+
+        let profile;
+        try {
+            profile = await this.userModel.findById(id);
+        } catch (e) {
+            throw new InternalError('Could not fetch user', e);
+        }
+        if (!profile) {
+            throw new NotFoundError('User not found');
+        }
+        const { password, ...parsedProfile } = profile;
+        return parsedProfile;
+    }
+
+    getProfileById = async (id) => {
+        let profile;
+        try {
+            profile = await this.userModel.findById(id);
+        } catch (e) {
+            throw new InternalError('Could not fetch user', e);
+        }
+        if (!profile) {
+            throw new NotFoundError('User not found');
+        }
+        const { password, role, roleLevel, isVerified, ...parsedProfile } = profile;
+        return parsedProfile;
+    }
+
+    updateEmail = async (id, password, newEmail) => {
+        const userCredentials = await this.#fetchAndVerifyUser(id, password);
+
+        if (userCredentials.email === newEmail.toLowerCase()) {
+            throw new ConflictError('Cannot use the same email');
+        }
+
+        try {
+            return await this.userModel.updateUserEmail(id, newEmail);
+        } catch (e) {
+            if (e.code === '23505') { throw new ConflictError('Email already in use') }
+            throw new InternalError('Could not update email', e);
+        }
+    }
+
+    updatePassword = async (id, password, newPassword) => {
+        const userCredentials = await this.#fetchAndVerifyUser(id, password);
+
+        const isSamePassword = await argon2.verify(userCredentials.password, newPassword);
+        if (isSamePassword) {
+            throw new ConflictError('Cannot use the same password');
+        }
+
+        try {
+            return await this.userModel.updateUserPassword(id, await argon2.hash(newPassword));
+        } catch (e) {
+            throw new InternalError('Could not update password', e);
+        }
+    }
+
+    updateUsername = async (id, password, newUsername) => {
+        const userCredentials = await this.#fetchAndVerifyUser(id, password);
+
+        if (userCredentials.username === newUsername) {
+            throw new ConflictError('Cannot use the same username');
+        }
+
+        try {
+            return await this.userModel.updateUserName(id, newUsername);
+        } catch (e) {
+            if (e.code === '23505') { throw new ConflictError('Username already in use') }
+            throw new InternalError('Could not update username', e);
+        }
     }
 }
