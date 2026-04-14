88cde2d91219dc9062f77e9d61c989d4ad9ca00d
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-04-14 13:20:07 +0200

    Refactor of the whole service
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- api/games/games.service.js
+++ api/games/games.service.js
@@ -1,15 +1,30 @@
-import { GameModel } from '../../shared/models/game.model.js';
+import { InternalError, NotFoundError } from "../../shared/middlewares/error.middleware.js";
+
 
 export class GameService {
-    constructor({} = {}) {
 
+    constructor({ GameModel } = {}) {
+        this.gameModel = GameModel;
     }
 
-    async getGames(params) {
-        return await GameModel.getGames(params);
+    async getAllGames(filters) {
+        try {
+            return await this.gameModel.findAllGames(filters);
+        } catch (e) {
+            throw new InternalError('Failed to fetch games', e);
+        }
     }
 
     async getGameBySlug(slug) {
-        return await GameModel.getGameBySlug(slug);
+        let game;
+        try {
+            game = await this.gameModel.findGameBySlug(slug);
+        } catch (e) {
+            throw new InternalError('Failed to fetch game', e);
+        }
+        if (!game) {
+            throw new NotFoundError('Game not found');
+        }
+        return game;
     }
 }
