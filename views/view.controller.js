7fdb07ffd987260946ad3ff40c63ebf75125af27
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-05-12 20:01:41 +0200

    Hotfix import
    Signed-off-by: Álvaro Gómez <alvaro.ongit@outlook.com>

--- views/view.controller.js
+++ views/view.controller.js
@@ -1,7 +1,7 @@
 /**
  * Controller responsible for rendering server-side views (EJS templates) in the application.
  *
- */ import {response} from "express";
+ */
 
 const BASE_URL = process.env.BASE_URL;
 
